af546ee76de4c5e2c00d987c0de495e819a8346b
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-23 00:01:48 +0300

    deplayment

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -4,16 +4,15 @@ import { initializeApp } from "firebase/app";
 // Import the Authentication and Firestore SDKs (In order to use these services)
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxGxcmVJLyqC-gu6RyKrqu8rdbAHWmBfE",
-  authDomain: "clone-9679c.firebaseapp.com",
-  projectId: "clone-9679c",
-  storageBucket: "clone-9679c.firebasestorage.app",
-  messagingSenderId: "80104195222",
-  appId: "1:80104195222:web:7bf9ccb9896ed805c0aeb7",
-  measurementId: "G-3TC9CXQZ35",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
